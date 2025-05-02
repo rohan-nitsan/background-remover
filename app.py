ce95ebdda1939fa750118d60b9a9c4337aab1232
Rohan Parmar <rohan.nitsan@gmail.com>
2025-05-02 16:48:04 +0530

    Update Post

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def server_error(error):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Fallback to 5000 locally
+    app.run(host='0.0.0.0', port=port)
